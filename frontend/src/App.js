6f85997c8477f4bab9b46f74d3b40ef834871d61
Chris <csluu@protonmail.com>
2023-02-24 02:41:51 -0500

    Was able to figure out useInView
    useInView from framer motion didn't work properly had to use react-intersection-observer instead

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,30 +7,52 @@ import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import { useRef } from "react";
+import { useInView } from "react-intersection-observer";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState("About");
   const isDesktop = useMediaQuery("(min-width: 768px)");
+  const [aboutMeRef, aboutMeInView] = useInView({
+    threshold: 0.5,
+  });
+  const [projectsRef, projectsInView] = useInView({
+    threshold: 0.5,
+  });
+  const [skillsRef, skillsInView] = useInView({
+    threshold: 0.5,
+  });
+  const [contactRef, contactInView] = useInView({
+    threshold: 0.5,
+  });
 
   useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY === 0) {
-        setSelectedPage("About");
-      }
-    };
-    // need to add this or else window.scrolly won't work
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+    if (aboutMeInView) {
+      setSelectedPage("About");
+    } else if (projectsInView) {
+      setSelectedPage("Projects");
+    } else if (skillsInView) {
+      setSelectedPage("Skills");
+    } else if (contactInView) {
+      setSelectedPage("Contact");
+    }
+  }, [aboutMeInView, projectsInView, skillsInView, contactInView]);
 
   return (
     <div className="App">
       <div className="fixed top-0 z-[-1] w-full h-full bg-linux saturate-50"></div>
       <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
-      <AboutMe />
-      <Projects />
-      <Skills />
-      <Contact />
+      <div ref={aboutMeRef}>
+        <AboutMe />
+      </div>
+      <div ref={projectsRef}>
+        <Projects />
+      </div>
+      <div ref={skillsRef}>
+        <Skills />
+      </div>
+      <div ref={contactRef}>
+        <Contact />
+      </div>
       <Footer />
     </div>
   );
